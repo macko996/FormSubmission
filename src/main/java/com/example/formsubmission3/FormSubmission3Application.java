07d467abb784e1bee74418362d540dc783f2d66a
Marjan Kolevski <Marjan.Kolevski@netcetera.com>
2020-05-21 09:22:08 +0200

    REST api with html text fields

---
+++ src/main/java/com/example/formsubmission3/FormSubmission3Application.java
@@ -0,0 +1,13 @@
+package com.example.formsubmission3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormSubmission3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormSubmission3Application.class, args);
+	}
+
+}
